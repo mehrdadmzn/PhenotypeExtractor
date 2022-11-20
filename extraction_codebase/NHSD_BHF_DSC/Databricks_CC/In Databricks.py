5e197ef91d82b7fbf4d936e0ba824d8c155b7c5e
mehrdadmizani@gmail.com <mehrdadmizani@gmail.com>
2022-11-20 01:38:53 +0000

    Different cells in databricks, test

--- extraction_codebase/NHSD_BHF_DSC/Databricks_CC/In Databricks.py
+++ extraction_codebase/NHSD_BHF_DSC/Databricks_CC/In Databricks.py
@@ -11,3 +11,17 @@ for x in [1, 2]:
 
 for y in ["3", "5"]:
     print(y)
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC # Test markdown
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC select  1;
+
+# COMMAND ----------
+
+
